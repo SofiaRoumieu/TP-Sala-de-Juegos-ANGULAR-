10afb3fdb9d7c6375a9ec9051781151d468d3f98
SofiaRoumieu <roumieusofia@gmail.com>
2020-09-07 19:58:11 -0300

    arreglos codigo

--- src/app/clases/juego-agilidad.ts
+++ src/app/clases/juego-agilidad.ts
@@ -1,2 +1,6 @@
+import { Juego } from '../clases/juego'
+
 export class JuegoAgilidad {
+    numeroIngresado = 0;
+    gano: boolean;
 }
--- src/app/componentes/mapa-de-google/mapa-de-google.component.ts
+++ src/app/componentes/mapa-de-google/mapa-de-google.component.ts
@@ -9,6 +9,7 @@ export class MapaDeGoogleComponent implements OnInit {
   title: string = 'El Primer mapa';
   lat: number = 33.678418;
   lng: number = 5.809007;
+ 
    // google maps zoom level
    zoom: number = 8;
    
@@ -20,7 +21,7 @@ export class MapaDeGoogleComponent implements OnInit {
   ngOnInit() {
   }
 
-
+  public mapClicked(evento:Event) {}
   
 
 
